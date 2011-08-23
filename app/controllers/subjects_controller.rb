1c2519d67d9ab65b947bf3e0d3b80de249b4819e
Leo Picado <leopic@gmail.com>
2011-08-22 23:41:18 -0600

    CRUD., 9.9

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,67 @@
+class SubjectsController < ApplicationController
+	
+	def index
+		list
+		render("list")
+	end
+	
+	def list
+		@subjects = Subject.order("subjects.position ASC")
+	end
+	
+	def show
+		@subject = Subject.find(params[:id])
+	end
+	
+	def new
+		#@subject = Subject.new(:name => "default")
+	end
+	
+	def create
+		#nueva instancia usando parametros
+		@subject = Subject.new(params[:subject])
+		#si el save funciona
+		if @subject.save
+			#redirecciona
+			flash[:notice] = "Subject created succesfully."
+			redirect_to(:action => "list")
+		else
+			#sino, lo manda de nuevo al new
+			render("new")
+		end
+	end
+	
+	def edit
+		@subject = Subject.find(params[:id])
+	end	
+	
+	def update				
+		#nueva instancia usando parametros
+		@subject = Subject.find(params[:id])
+		#si el update funca
+		if @subject.update_attributes(params[:subject])
+			#redirecciona
+			flash[:notice] = "Subject edited succesfully."
+			redirect_to(:action => "show", :id => @subject.id)
+		else
+			#sino, lo manda de nuevo al edit
+			render("edit")
+		end
+	end
+	
+	def delete
+		@subject = Subject.find(params[:id])
+	end
+	
+	def destroy
+		#nueva instancia usando parametros
+		#subject = Subject.find(params[:id])
+		Subject.find(params[:id]).destroy
+		#como no vamos a hacer nada con el objeto
+		#no ocupamos instanciarlo/mostrarlo, nos ahorramos
+		#la variable de instancia		
+		flash[:notice] = "Subject destroyed succesfully."
+		redirect_to(:action => "list")
+	end
+	
+end
