d6114a3bc479f0a1f8f265dfd914a1deea1e31c6
Leo Picado <leopic@gmail.com>
2011-09-16 23:14:33 -0600

    Validations, server side.

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,5 +5,10 @@ class Page < ActiveRecord::Base
 	has_and_belongs_to_many :editors, :class_name => "AdminUser"
 	
 	validates_presence_of :name
+	validates_length_of_ :name, :maximum => 255
+	validates_presence_of :permalink
+	validates_length_of_ :permalink, :within => 3..255
+	# se ocupa el within, para que el presence no sean solo espacios
+	validates_uniqueness_of :permalink
 	
 end
